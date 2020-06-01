70f1d527666c8d22096677702b4902fa40fab8a7
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-06-01 23:25:57 +0900

    コンポーネントの基本 完全に理解

--- 08_component.js
+++ 08_component.js
@@ -5,3 +5,36 @@ Vue.component('blog-post', {
 const component1 = new Vue({
   el: '#component1',
 });
+
+Vue.component('blog-item', {
+  props: ['title'],
+  template: `
+    <div>
+      <h3>{{ title }}</h3>
+      <button v-on:click="$emit('count', 1)">いいね！</button>
+    </div>
+  `,
+});
+const component2 = new Vue({
+  el: '#component2',
+  data: {
+    posts: [
+      { id: 0, title: 'Blog1' },
+      { id: 1, title: 'Blog2' },
+      { id: 2, title: 'Blog3' },
+    ],
+    count: 0,
+  },
+});
+
+Vue.component('alert-item', {
+  template: `
+    <div style="color: red;">
+      <strong>Error: </strong>
+      <slot><!-- ここに任意のコンテンツが挿入されます --></slot>
+    </div>
+  `,
+});
+const component3 = new Vue({
+  el: '#component3',
+});
