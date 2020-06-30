77cb024f6ca87475bc83f01cac8a8c6a860a0e54
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-07-01 00:39:50 +0900

    プログラムによるナビゲーション 完全に理解

--- vue-router/src/components/User.vue
+++ vue-router/src/components/User.vue
@@ -2,11 +2,30 @@
   <div>
     <p>User [{{ this.$route.params.id }}]</p>
     <router-view/>
+    <button @click="previous">Previous</button>
+    <button @click="top">Top</button>
+    <button @click="next">Next</button>
   </div>
 </template>
 
 <script>
+import router from '@/router/index.js'
+
 export default {
-  name: 'User'
+  name: 'User',
+  methods: {
+    previous: function () {
+      // ページ遷移のヒストリーを一つ戻す
+      router.go(-1)
+    },
+    next: function () {
+      // ページ遷移のヒストリーを一つ進める
+      router.go(1)
+    },
+    top: function () {
+      // JSからもルーターに従ったページ遷移を行うことができる
+      router.push({ path: '/' })
+    }
+  }
 }
 </script>
