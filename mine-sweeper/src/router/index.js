b583aabf67c1e7ec74e750dbd4ada69fa2bce3ca
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-07-05 04:12:49 +0900

    ルーティングに応じてページタイトルを更新するようにした

--- mine-sweeper/src/router/index.js
+++ mine-sweeper/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 import Game from '../views/Game'
 
 Vue.use(VueRouter)
@@ -9,14 +8,27 @@ const routes = [
   {
     path: '/',
     name: 'Game',
-    component: Game
-  }
+    component: Game,
+    meta: {
+      title: 'マインスイーパー',
+    },
+  },
+  {
+    // リダイレクト: すべてルートに戻す
+    path: '*',
+    redirect: { name: 'Game' }
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title
+  next()
 })
 
 export default router
