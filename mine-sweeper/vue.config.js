6ba8ec8fcb37febe0bb42bc9b031867cd621a020
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-07-07 02:36:19 +0900

    マインスイーパーのテンプレートHTMLを更新、初回表示でタイトルが正しく表示されるようになった

--- mine-sweeper/vue.config.js
+++ mine-sweeper/vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   publicPath: './',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].template = './src/index.html'
+        return args
+      })
+  },
 }
